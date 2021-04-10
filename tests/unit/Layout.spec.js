2dab87b510d82e088602f321dbe5ca9ea977d8a9
sbenitezma <sbenitezma@gmail.com>
2021-04-10 21:43:56 +0200

    Fix Layout tests

--- tests/unit/Layout.spec.js
+++ tests/unit/Layout.spec.js
@@ -4,23 +4,24 @@ import Vuetify from "vuetify";
 import Layout from "@/components/Layout.vue";
 import TabsMenu from "@/components/TabsMenu";
 
-const localVue = createLocalVue();
-localVue.use(Vuex);
-
 describe.only("Layout.vue", () => {
   const localVue = createLocalVue();
-  let mutations;
-  let store;
   let vuetify;
   let wrapper;
+
+  let store;
+  let actions;
+
   vuetify = new Vuetify();
+  localVue.use(Vuex);
 
   beforeEach(() => {
+    actions = {
+      LOAD_APPS: jest.fn(),
+    };
     store = new Vuex.Store({
-      state: {},
-      mutations,
+      actions,
     });
-    wrapper = shallowMount(Layout, { store, vuetify, localVue });
   });
   // Destroy the component wrapper after each test
   afterEach(() => {
@@ -28,10 +29,22 @@ describe.only("Layout.vue", () => {
   });
 
   it("Renders Layout component", () => {
+    wrapper = shallowMount(Layout, { store, vuetify, localVue });
     expect(wrapper.findComponent(Layout).exists()).toBeTruthy();
   });
 
   it("Renders TabsMenu component", () => {
+    wrapper = shallowMount(Layout, { store, vuetify, localVue });
     expect(wrapper.findComponent(TabsMenu).exists()).toBeTruthy();
+  });
+
+  it("Expect fetchApps mounted action being called", async () => {
+    const fetchAction = jest.spyOn(Layout.methods, "fetchApps");
+    wrapper = shallowMount(Layout, {
+      store,
+      vuetify,
+      localVue,
+    });
+    expect(fetchAction).toHaveBeenCalled();
   });
 });
